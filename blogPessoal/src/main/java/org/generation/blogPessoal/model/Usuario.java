992ff79fd703ccd077a3dc86b9246c078b964945
Robson Coura <robson_coura@yahoo.com.br>
2025-07-31 15:52:50 -0300

    feat(Usuario): Add entidade Usuário com as anotações da JPA

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -0,0 +1,61 @@
+package org.generation.blogPessoal.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+
+    //Atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    @Size(min = 2, max = 100)
+    private String nome;
+
+    @NotNull
+    @Size(min = 5, max = 100)
+    private String usuario;
+
+    @NotNull
+    @Size(min = 5, max = 100)
+    private String senha;
+
+    //Método recuperador e modificador de atributo
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+}
