6e53aedbec5cd2b15c7f1bb3a5fa59c5a442f2b7
Robson Coura <robson_coura@yahoo.com.br>
2025-07-31 13:46:38 -0300

    feat(PostagemController): Criação do endpoint para cadastrar uma Postagem

--- blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -3,6 +3,7 @@ package org.generation.blogPessoal.controller;
 import org.generation.blogPessoal.model.Postagem;
 import org.generation.blogPessoal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -37,4 +38,9 @@ public class PostagemController {
         return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
     }
 
+    //Método para cadastrar uma postagem
+    @PostMapping
+    public ResponseEntity<Postagem> post(@RequestBody Postagem postagem){
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+    }
 }
