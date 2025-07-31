d71a9b0b53d32f62ed00d1b9a1033400b69e50b2
Robson Coura <robson_coura@yahoo.com.br>
2025-07-31 09:13:35 -0300

    feat(PostagemRepository): Adicionei o método buscar todas postagem pelo titulo

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/repository/PostagemRepository.java
@@ -0,0 +1,15 @@
+package org.generation.blogPessoal.repository;
+
+import org.generation.blogPessoal.model.Postagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+
+    //Método buscar todos pelo titulo
+    public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+
+}
