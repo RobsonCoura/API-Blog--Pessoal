0ee8cf3638322d6bc540017f20c4213ac3510f37
Robson Coura <robson_coura@yahoo.com.br>
2025-07-30 21:30:07 -0300

    feat(Postagem, Tema): Adicionar entidade Postagem e Tema com as anotações JPA
    Introduces the Postagem and Tema classes as JPA entities for the blog application. Postagem includes fields for title, text, date, and a many-to-one relationship with Tema, while Tema manages a list of associated Postagem objects. These models support validation and JSON serialization for RESTful operations.

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Tema.java
@@ -0,0 +1,48 @@
+package org.generation.blogPessoal.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_tema")
+public class Tema {
+
+    //Atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @NotNull
+    private String descricao;
+
+    @OneToMany(mappedBy = "tema", cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("tema")
+    private List<Postagem> postagem;
+
+    //Método de recuperar e modificar atributos
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Postagem> getPostagem() {
+        return postagem;
+    }
+
+    public void setPostagem(List<Postagem> postagem) {
+        this.postagem = postagem;
+    }
+}
