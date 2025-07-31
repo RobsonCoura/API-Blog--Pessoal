de91c17f3a7c26a960808a6c15c2201a91b20ec9
Robson Coura <robson_coura@yahoo.com.br>
2025-07-31 15:59:41 -0300

    feat(UserLogin): Adicionar entidade UserLogin

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/UserLogin.java
@@ -0,0 +1,46 @@
+package org.generation.blogPessoal.model;
+
+public class UserLogin {
+
+    //Atributos
+    private String nome;
+
+    private  String usuario;
+
+    private String senha;
+
+    private String token;
+
+    //Método modificador e recuperador de atributos
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
